0e9ff4a4bd4b3a7045f9e62c904c08a057009e4e
Danny Qiu <dqiu55@gmail.com>
2017-12-22 00:49:09 -0500

    Add day01

---
+++ 2017/day01/p2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import sys
+
+def solve(data):
+    digits = map(int, list(data))
+    total = 0
+    for i in range(len(digits)):
+        if digits[i] == digits[(i + len(digits)/2) % len(digits)]:
+            total += digits[i]
+    print(total)
+
+if __name__ == "__main__":
+    data = sys.stdin.read()
+    data = data.strip()
+    solve(data)
