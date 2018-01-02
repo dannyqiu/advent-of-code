9cb7d2a906c03e1c76cba6cf3c6cc31b70e41941
Danny Qiu <dqiu55@gmail.com>
2018-01-02 03:00:18 -0500

    Make day 1 executable

--- 2017/day01/p2.py
+++ 2017/day01/p2.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from __future__ import print_function
 import sys
 
 def solve(data):
-    digits = map(int, list(data))
+    digits = list(map(int, list(data)))
     total = 0
     for i in range(len(digits)):
-        if digits[i] == digits[(i + len(digits)/2) % len(digits)]:
+        if digits[i] == digits[(i + len(digits)//2) % len(digits)]:
             total += digits[i]
-    print(total)
+    print("New captcha solution:", total)
 
 if __name__ == "__main__":
     data = sys.stdin.read()
