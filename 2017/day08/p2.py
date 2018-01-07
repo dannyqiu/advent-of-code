76bc4dd102b8f90dc3c76df134590bcd184428a7
Danny Qiu <dqiu55@gmail.com>
2018-01-07 17:59:13 -0500

    Add day 8

---
+++ 2017/day08/p2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import sys
+
+DEFAULT_VALUE = 0
+
+def ensure_reg(registers, reg):
+    if reg not in registers:
+        registers[reg] = DEFAULT_VALUE
+    return registers[reg]
+
+def verify_cond(registers, reg_cond, cond, value_cond):
+    reg_value = ensure_reg(registers, reg_cond)
+    stmt = '{} {} {}'.format(reg_value, cond, value_cond)
+    return eval(stmt)
+
+def run_code(registers, instructions):
+    highest = DEFAULT_VALUE
+    for instr in instructions:
+        tokens = instr.split()
+        reg = tokens[0]
+        is_inc = tokens[1] == "inc"
+        amount = int(tokens[2])
+        reg_cond = tokens[4]
+        cond = tokens[5]
+        value_cond = int(tokens[6])
+        if verify_cond(registers, reg_cond, cond, value_cond):
+            reg_value = ensure_reg(registers, reg)
+            registers[reg] = reg_value + amount * (1 if is_inc else -1)
+        highest = max(highest, max(registers.values() if registers else DEFAULT_VALUE))
+    return highest
+
+def solve(data):
+    registers = {}
+    instructions = data.split('\n')
+    highest = run_code(registers, instructions)
+    print("Highest value in any register during process:", highest)
+
+if __name__ == "__main__":
+    data = sys.stdin.read()
+    data = data.strip()
+    solve(data)
